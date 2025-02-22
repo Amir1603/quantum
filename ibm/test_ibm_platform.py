80bd1cc6539e1f06ca35db60710b7962b94aa062
Amir1603 <53334966+Amir1603@users.noreply.github.com>
2025-02-22 18:07:26 +0200

    Change to SimplerV2 no backend (#2)

--- ibm/test_ibm_platform.py
+++ ibm/test_ibm_platform.py
@@ -5,7 +5,6 @@ from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 example_circuit = QuantumCircuit(2)
 example_circuit.measure_all()
 
-# You'll need to specify the credentials when initializing QiskitRuntimeService, if they were not previously saved.
 service = QiskitRuntimeService()
 backend = service.least_busy(operational=True, simulator=False)
 
