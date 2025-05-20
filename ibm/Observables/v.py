0cd3b067c26fbb86d9f4959bbd9027cf68d01cdb
Amir Yona <yonaamir@gmail.com>
2025-05-20 12:15:06 +0300

    Remove duplicated get_value method in v

--- ibm/Observables/v.py
+++ ibm/Observables/v.py
@@ -23,34 +23,16 @@ class V_B(EnergyBase):
             return "X"
 
     def get_value(self, bitstring: str):
-        bob_bit = utils.get_bit_from_counts(bitstring, utils.get_bob_idx(self.N), self.N)
-        bob_neighbour_bit = utils.get_bit_from_counts(bitstring, utils.get_bob_neighbor_idx(self.N), self.N)
+        bob_idx = utils.get_bob_idx(self.N)
+        bob_neighbor_idx = utils.get_bob_neighbor_idx(self.N)
+
+        bob_bit = utils.get_bit_from_counts(bitstring, bob_idx, self.N)
+        bob_neighbour_bit = utils.get_bit_from_counts(bitstring, bob_neighbor_idx, self.N)
 
         bob_neighbour_val = 1 if bob_neighbour_bit == '0' else -1
         bob_val = 1 if bob_bit == '0' else -1
 
-        # Value is product of eigenvalues
         return bob_neighbour_val * bob_val
-
-    def get_value(self, bitstring: str):
-        """
-        Calculates eigenvalue of X_{N-2}*X_{N-1} (+1 or -1) from Z_{N-2}, Z_{N-1} measurement outcomes...
-        """
-        try:
-            intermed_creg = utils.get_bob_neighbor_idx(self.N)
-            bob_creg = utils.get_bob_idx(self.N)
-
-            # Ensure get_bit_from_counts is used correctly
-            z1_meas_bit = utils.get_bit_from_counts(bitstring, intermed_creg, self.N)
-            z2_meas_bit = utils.get_bit_from_counts(bitstring, bob_creg, self.N)
-
-            z1_eigenvalue = 1.0 if z1_meas_bit == '0' else -1.0
-            z2_eigenvalue = 1.0 if z2_meas_bit == '0' else -1.0
-
-            return z1_eigenvalue * z2_eigenvalue
-        except IndexError as e:
-            print(f"Error in {self.name}.get_value: Bitstring '{bitstring}', {e}")
-            raise e # Or return 0.0
 
     def description(self):
         bob_idx = utils.get_bob_idx(self.N)
