Fix Numerical class to create the same types as analytical class

ibm/Calculators/numerical_tfim.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.sparse import kron, eye, csc_matrix
 from scipy.sparse.linalg import eigsh
 import utils
+from qiskit.quantum_info import Statevector, DensityMatrix
 from .tfim_calculator import TFIMCalculator
 
 class NumericalTFIM(TFIMCalculator):
@@ -24,7 +25,9 @@ class NumericalTFIM(TFIMCalculator):
                density matrix of the ground state, total energy, total charge,
                expectation values of Z and XX operators.
         """
-        self.E0, self.gs0, self.E1, self.ex1 = self._compute_lowest_states()
+        self.E0, gs0, self.E1, ex1 = self._compute_lowest_states()
+
+        self.gs0, self.ex1 = Statevector(gs0), Statevector(ex1)
 
         # Compute density matrices
         self.gs_rho = NumericalTFIM._compute_density_matrix(self.gs0)
@@ -58,7 +61,7 @@ class NumericalTFIM(TFIMCalculator):
 
     # Density Matrix Calculation
     def _compute_density_matrix(state):
-        return np.outer(state, np.conj(state))
+        return DensityMatrix(state)
 
     # Helper function to create a Pauli operator on a specific site
     def _get_pauli_operator_on_site(op_char, site_idx, N):
@@ -91,7 +94,7 @@ class NumericalTFIM(TFIMCalculator):
 
     # Helper to compute expectation value <gs|Op|gs>
     def _compute_expectation_value(op_matrix, gs):
-        gs_col_sparse = csc_matrix(gs.reshape(-1, 1))
+        gs_col_sparse = csc_matrix(gs.data.reshape(-1, 1))
         if not isinstance(op_matrix, csc_matrix):
             op_matrix = csc_matrix(op_matrix)
         val = gs_col_sparse.conj().T @ op_matrix @ gs_col_sparse
@@ -145,7 +148,7 @@ class NumericalTFIM(TFIMCalculator):
     # Bob's Energy and Charge Expectation Calculation
 
     def _compute_bob_gs_energy_and_charge(self):
-        gs = csc_matrix(self.gs0.reshape(-1, 1))
+        gs = csc_matrix(self.gs0.data.reshape(-1, 1))
 
         bob_site = utils.get_bob_idx(self.N)
         bob_neighbor_site = utils.get_bob_neighbor_idx(self.N)