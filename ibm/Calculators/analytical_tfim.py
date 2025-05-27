Fix signs after fixing analytical derivation

ibm/Calculators/analytical_tfim.py
@@ -31,10 +31,10 @@ class AnalyticalTFIM(TFIMCalculator):
 
         k = self.J / 2
         self.theta_E1 = np.arcsin(
-                (self.h * k) / np.sqrt((self.h**2 + 2 * k**2)**2 + self.h**2 * k**2)
+                (-self.h * k) / np.sqrt((self.h**2 + 2 * k**2)**2 + self.h**2 * k**2)
             ) / 2
 
-        self.theta_q1 = 0.5 * np.arctan(-self.J / (2 * self.h))
+        self.theta_q1 = 0.5 * np.arctan(self.J / (2 * self.h))
 
         # FIXME: Complete after analyzing other Alice basis
         self.theta_E2, self.theta_q2 = 0.0, 0.0

ibm/Calculators/numerical_tfim.py
@@ -109,11 +109,11 @@ class NumericalTFIM(TFIMCalculator):
         Zbob_exp = NumericalTFIM._compute_expectation_value(op_Zbob, self.gs0)
         X0_Xbob_exp = NumericalTFIM._compute_expectation_value(op_X0_Xbob, self.gs0)
 
-        num_theta_E1 = -self.h * X0_Xbob_exp + self.J * Zbob_exp
+        num_theta_E1 = self.h * X0_Xbob_exp - self.J * Zbob_exp
         den_theta_E1 = self.h * Zbob_exp + self.J * X0_Xbob_exp
         theta_E1 = 0.5 * np.arctan(num_theta_E1 / den_theta_E1)
 
-        num_theta_q1 = -X0_Xbob_exp
+        num_theta_q1 = X0_Xbob_exp
         den_theta_q1 = Zbob_exp
         theta_q1 = 0.5 * np.arctan(num_theta_q1 / den_theta_q1)
 