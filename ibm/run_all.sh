91f76c7c56d536b1dae213ca5023a65bc5619abd
Amir Yona <yonaamir@gmail.com>
2025-05-28 21:53:31 +0300

    cleanup run_all script

--- ibm/run_all.sh
+++ ibm/run_all.sh
@@ -5,28 +5,14 @@ Ns=(1 2 3)
 
 for N in "${Ns[@]}"
 do
-    # Prefer analytical calculations if possible
-    if [ "$N" -eq 1 ]; then
-        dir="N$N-Analytical"
-        echo "Running: $dir"
-        echo | python main.py --run-analytical --both-alice-values -Js 40 -N $N -o $dir
-    else
-        dir="N$N-Numerical"
-        echo "Running: $dir"
-        echo | python main.py --both-alice-values -Js 40 -N $N -o $dir
-    fi
+    dir="N$N"
+    echo "Running: $dir"
+    echo | python main.py --both-alice-values -Js 100 -N $N -o $dir
 
     for err in "${errors[@]}"
     do
-        # Prefer analytical calculations if possible
-        if [ "$N" -eq 1 ]; then
-            dir="N$N-Analytical-$err"
-            echo "Running: $dir"
-            echo | python main.py --run-analytical -Js 4 -N $N --$err -o $dir
-        else
-            dir="N$N-Numerical-$err"
-            echo "Running: $dir"
-            echo | python main.py -Js 4 -N $N --$err -o $dir
-        fi
+        dir="N$N-$err"
+        echo "Running: $dir"
+        echo | python main.py -Js 4 -N $N --$err -o $dir --avoid_J0
     done
 done
