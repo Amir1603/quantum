2baf0ac7e03ccbed5786923aed615b6deb8c4f4b
Amir Yona <yonaamir@gmail.com>
2025-06-12 13:04:39 +0300

    Fix Js range in plot_theoretical_values since it slightly differs after removing the division by the ground state expectation value

--- ibm/plot_theoretical_values.py
+++ ibm/plot_theoretical_values.py
@@ -19,7 +19,7 @@ def get_unique_legend(ax_array):
 
 if __name__ == "__main__":
     Ns = [1, 2, 3, 4]
-    Js = np.linspace(0.0, 4.0, 100).tolist()
+    Js = np.linspace(0.0, 10.0, 250).tolist()
     h = 1.0
 
     # Raw figure - 5 subplots in a 2x3 grid, remove unused subplot
