c683359fe3acb916297c63730dd68c79e9b2313d
Amir Yona <yonaamir@gmail.com>
2025-07-08 18:05:28 +0300

    Update TODO

--- ibm/plot_theoretical_values.py
+++ ibm/plot_theoretical_values.py
@@ -119,6 +119,8 @@ if __name__ == "__main__":
 
     # TODO: Define run configuration class to hold these info instead of complicated dictionary.
     #       Add also option to control what steps to run - for example 'nn' doesn't need to run errors simulation.
+    #       This can also be massively optimized since now the calculation runs twice on nn_H just to plot once
+    #       both bases together and once each once separately.
     hamiltonians = {
         'alice': (Operators.alice_H, [Operators.alice_HB, Operators.QB], [utils.AliceBase.X], [1, 2, 3, 4]),
         'nn': (Operators.nn_H, [Operators.nn_HB, Operators.QB], [utils.AliceBase.X, utils.AliceBase.Y], [2, 3, 4]),
