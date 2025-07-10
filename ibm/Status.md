4700ba276975eda7cdb8281620e111d3eabb09fc
Amir1603 <53334966+Amir1603@users.noreply.github.com>
2025-07-10 11:46:10 +0300

    Improve numerical sim (#47)
    * Separate run configuration to dedicated class and rework usage to now have a solid boundaries for y limits in errors plots and avoid calculating multiple times the same system\observables
    
    * Remove done tasks from status

--- ibm/Status.md
+++ ibm/Status.md
@@ -1,13 +1,6 @@
 TODO:
 * From last meeting:
-1) What happens if Bob doesn't rotate? Does he get entirely ranfom without a defined sign or will he receive a defined expectation value?
-2) Try and create a better understanding of the scaling - how does the number of averaging shots needed depend on N? (Try bigger Ns with less points of J)
-3) Veify the influence of a shift to the operator:
-    3.1) Verify by analytical calculation.
-    3.2) Check results of a numeric simulation with and without the shift.
-4) Plot also the teleported value as function of h.
-5) Fix errors injection.
-6) Add a drawing of the circuit.
+1) Try and create a better understanding of the scaling - how does the number of averaging shots needed depend on N? (Try bigger Ns with less points of J)
 
 * For Yaron (Future):
 1) Plot together both Alice's bases in NN simulation.
--- ibm/plot_theoretical_values.py
+++ ibm/plot_theoretical_values.py
@@ -4,6 +4,7 @@ from Calculators import Operators, NumericalTFIM
 import utils
 from conf import Conf, ErrorsConf
 import os
+from numerical_run_conf import NumericalRunConf
 
 
 # Collect and deduplicate legend handles and labels
@@ -21,7 +22,7 @@ def get_unique_legend(ax_array):
     return unique_handles, unique_labels
 
 
-def single_run(conf: Conf, alice_base: utils.AliceBase, OB_classes: list[type]):
+def single_run(conf: Conf, alice_base: utils.AliceBase, hamiltonian_class: type, OB_classes: list[type]):
     OBs = {OB_class.__name__: OB_class(conf.h, conf.J, conf.N, alice_base) for OB_class in OB_classes}
     H = hamiltonian_class(conf.h, conf.J, conf.N)
 
@@ -30,13 +31,15 @@ def single_run(conf: Conf, alice_base: utils.AliceBase, OB_classes: list[type]):
 
     return OBs
 
-def run_Js(conf: Conf, i: int, N: int, alice_base: utils.AliceBase, OB_classes: list[type], tel_axis):
+def run_Js(i: int, N: int, alice_base: utils.AliceBase, hamiltonian_class: type, OB_classes: list[type], tel_axes):
     teleported_values = {}
-    confs = Conf.generate_J_for_h(conf, 100)
+    confs = Conf.generate_J_for_h(Conf(N), 100)
     Js = [c.J for c in confs]
 
+    print(f"Running all Js ({len(Js)})")
+
     for c in confs:
-        OBs = single_run(c, alice_base, OB_classes)
+        OBs = single_run(c, alice_base, hamiltonian_class, OB_classes)
 
         for k, v in OBs.items():
             if k not in teleported_values:
@@ -48,17 +51,20 @@ def run_Js(conf: Conf, i: int, N: int, alice_base: utils.AliceBase, OB_classes: 
         a0_vals = [tv[False] for tv in teleported_values[k]]
         a1_vals = [tv[True] for tv in teleported_values[k]]
 
-        tel_axis[idx, i].plot(Js, a0_vals, label=f'a=0, sigma_A={alice_base}')
-        tel_axis[idx, i].plot(Js, a1_vals, label=f'a=1, sigma_A={alice_base}')
-        tel_axis[idx, i].set_title(f"{k} N={N}")
+        for tel_axis in tel_axes:
+            tel_axis[idx, i].plot(Js, a0_vals, label=f'a=0, sigma_A={alice_base}')
+            tel_axis[idx, i].plot(Js, a1_vals, label=f'a=1, sigma_A={alice_base}')
+            tel_axis[idx, i].set_title(f"{k} N={N}")
 
-def run_hs(conf: Conf, i: int, N: int, alice_base: utils.AliceBase, OB_classes: list[type], tel_axis):
+def run_hs(i: int, N: int, alice_base: utils.AliceBase,  hamiltonian_class: type, OB_classes: list[type], tel_axes):
     teleported_values = {}
-    confs = Conf.generate_h_for_J(conf, 100)
+    confs = Conf.generate_h_for_J(Conf(N), 100)
     hs = [c.h for c in confs]
 
+    print(f"Running all hs ({len(hs)})")
+
     for c in confs:
-        OBs = single_run(c, alice_base, OB_classes)
+        OBs = single_run(c, alice_base, hamiltonian_class, OB_classes)
 
         for k, v in OBs.items():
             if k not in teleported_values:
@@ -70,33 +76,28 @@ def run_hs(conf: Conf, i: int, N: int, alice_base: utils.AliceBase, OB_classes: 
         a0_vals = [tv[False] for tv in teleported_values[k]]
         a1_vals = [tv[True] for tv in teleported_values[k]]
 
-        tel_axis[idx, i].plot(hs, a0_vals, label=f'a=0, sigma_A={alice_base}')
-        tel_axis[idx, i].plot(hs, a1_vals, label=f'a=1, sigma_A={alice_base}')
-        tel_axis[idx, i].set_title(f"{k} N={N}")
+        for tel_axis in tel_axes:
+            tel_axis[idx, i].plot(hs, a0_vals, label=f'a=0, sigma_A={alice_base}')
+            tel_axis[idx, i].plot(hs, a1_vals, label=f'a=1, sigma_A={alice_base}')
+            tel_axis[idx, i].set_title(f"{k} N={N}")
 
-errors = {
-    'p_classical_error': (ErrorsConf.generate_classical_error, None),
-    'p_depol_error': (ErrorsConf.generate_depolarization_error, None),
-    'p_bitflip_error': (ErrorsConf.generate_bitflip_error, [(-0.1, 0.5), None]),
-    'p_alice_phaseflip_error': (ErrorsConf.generate_alice_phase_flip_error, [(-0.1, 0.5), None]),
-    'p_bob_phaseflip_error': (ErrorsConf.generate_bob_phase_flip_error, [(-0.1, 0.5), None]),
-    'p_excited_mixture_error': (ErrorsConf.generate_excited_mixture_error, [(-0.1, 0.2), (-2, 1.5)]),
-    'p_excited_superposition_error': (ErrorsConf.generate_excited_superposition_error, [(-0.1, 0.2), (-2, 1.5)]),
-}
-
-def run_errors(conf: Conf, i: int, N: int, alice_base: utils.AliceBase, OB_classes: list[type], errs_axis):
+def run_errors(i: int, N: int, alice_base: utils.AliceBase,  hamiltonian_class: type, OB_classes: list[type], errs_axis):
     Js = np.linspace(1.0, 4.0, 7).tolist()
 
-    for j, (name, (generate_err, ylim)) in enumerate(errors.items()):
+    print("Running all errors")
+
+    for j, err_name in enumerate(NumericalRunConf.get_errors()):
+        conf = Conf(N)
+
         for J in Js:
             conf.J = J
             teleported_values = {}
 
-            errs = generate_err()
+            errs = NumericalRunConf.generate_errors_configurations(err_name)
             confs = [c for c in Conf.generate_from_errors(conf, errs)]
 
             for c in confs:
-                OBs = single_run(c, alice_base, OB_classes)
+                OBs = single_run(c, alice_base,  hamiltonian_class, OB_classes)
 
                 for k, v in OBs.items():
                     if k not in teleported_values:
@@ -107,85 +108,101 @@ def run_errors(conf: Conf, i: int, N: int, alice_base: utils.AliceBase, OB_class
             for idx, k in enumerate(teleported_values):
                 a0_vals = [tv[False] for tv in teleported_values[k]]
 
-                p_errs = [err_conf.__dict__[name] for err_conf in errs]
+                p_errs = [err_conf.__dict__[err_name] for err_conf in errs]
                 errs_axis[j][idx, i].plot(p_errs, a0_vals, label=f'J={J}')
+
+                ylim = NumericalRunConf.get_ylim(err_name, k)
                 if ylim and ylim[idx]:
                     errs_axis[j][idx, i].set_ylim(ylim[idx])
                 errs_axis[j][idx, i].set_title(f"{k} N={N}")
+                errs_axis[j][idx, i].axvline(x=0, color='black', linestyle='--')
+                errs_axis[j][idx, i].axhline(y=0, color='black', linestyle=':')
 
 
 if __name__ == "__main__":
-    h = 1.0
+    run_confs = [
+        NumericalRunConf.AliceConf(),
+        NumericalRunConf.NNConf(),
+    ]
 
-    # TODO: Define run configuration class to hold these info instead of complicated dictionary.
-    #       Add also option to control what steps to run - for example 'nn' doesn't need to run errors simulation.
-    #       This can also be massively optimized since now the calculation runs twice on nn_H just to plot once
-    #       both bases together and once each once separately.
-    hamiltonians = {
-        'alice': (Operators.alice_H, [Operators.alice_HB, Operators.QB], [utils.AliceBase.X], [1, 2, 3, 4]),
-        'nn': (Operators.nn_H, [Operators.nn_HB, Operators.QB], [utils.AliceBase.X, utils.AliceBase.Y], [2, 3, 4]),
-        'nn_X': (Operators.nn_H, [Operators.nn_HB, Operators.QB], [utils.AliceBase.X], [2, 3, 4]),
-        'nn_Y': (Operators.nn_H, [Operators.nn_HB, Operators.QB], [utils.AliceBase.Y], [2, 3, 4]),
-    }
-
-    for name, (hamiltonian_class, OB_classes, alice_bases, Ns) in hamiltonians.items():
-        # Teleported figure
-        J_tel_figure, J_tel_axis = plt.subplots(2, len(Ns), figsize=(10, 6))
-        J_tel_figure.subplots_adjust(right=0.8)
-        for ax in J_tel_axis.flat:
+    for run_conf in run_confs:
+        # Teleported figure for both bases
+        J_tel_figure_both_bases, J_tel_axis_both_bases = plt.subplots(2, len(run_conf.Ns), figsize=(10, 6))
+        J_tel_figure_both_bases.subplots_adjust(right=0.8)
+        for ax in J_tel_axis_both_bases.flat:
             ax.set_title("", fontsize=10)
 
-        h_tel_figure, h_tel_axis = plt.subplots(2, len(Ns), figsize=(10, 6))
-        h_tel_figure.subplots_adjust(right=0.8)
-        for ax in h_tel_axis.flat:
+        h_tel_figure_both_bases, h_tel_axis_both_bases = plt.subplots(2, len(run_conf.Ns), figsize=(10, 6))
+        h_tel_figure_both_bases.subplots_adjust(right=0.8)
+        for ax in h_tel_axis_both_bases.flat:
             ax.set_title("", fontsize=10)
 
-        # Errors figure
-        errs_plot = {}
-        for err_name in errors.keys():
-            err_figure, err_axis = plt.subplots(2, len(Ns), figsize=(10, 6))
-            err_figure.subplots_adjust(right=0.8)
-            for ax in err_axis.flat:
+        for alice_base in run_conf.alice_bases:
+            # Teleported figure
+            J_tel_figure, J_tel_axis = plt.subplots(2, len(run_conf.Ns), figsize=(10, 6))
+            J_tel_figure.subplots_adjust(right=0.8)
+            for ax in J_tel_axis.flat:
                 ax.set_title("", fontsize=10)
-            
-            errs_plot[err_name] = err_figure, err_axis
 
-        print(f"Calculating theoretical values for {name}...")
+            h_tel_figure, h_tel_axis = plt.subplots(2, len(run_conf.Ns), figsize=(10, 6))
+            h_tel_figure.subplots_adjust(right=0.8)
+            for ax in h_tel_axis.flat:
+                ax.set_title("", fontsize=10)
 
-        for alice_base in alice_bases:
-            for i, N in enumerate(Ns):
+            # Errors figure
+            errs_plot = {}
+            for err_name in NumericalRunConf.get_errors():
+                err_figure, err_axis = plt.subplots(2, len(run_conf.Ns), figsize=(10, 6))
+                err_figure.subplots_adjust(right=0.8)
+                for ax in err_axis.flat:
+                    ax.set_title("", fontsize=10)
+                
+                errs_plot[err_name] = err_figure, err_axis
+
+            print(f"Calculating theoretical values for {run_conf.name}...")
+
+            for i, N in enumerate(run_conf.Ns):
                 print(f"Calculating for N={N} with AliceBase={alice_base}...")
 
-                conf = Conf(N)
-                conf.h = h
-                conf.xor_alice_res = 0
-                conf.errors = ErrorsConf()
-
-                run_Js(conf, i, N, alice_base, OB_classes, J_tel_axis)
-                run_hs(conf, i, N, alice_base, OB_classes, h_tel_axis)
+                run_Js(i, N, alice_base, run_conf.H_type, run_conf.OB_types, [J_tel_axis, J_tel_axis_both_bases])
+                run_hs(i, N, alice_base, run_conf.H_type, run_conf.OB_types, [h_tel_axis, h_tel_axis_both_bases])
 
                 errs_axis = [err_axis for _, err_axis in errs_plot.values()]
-                run_errors(conf, i, N, alice_base, OB_classes, errs_axis)
+                run_errors(i, N, alice_base, run_conf.H_type, run_conf.OB_types, errs_axis)
 
-        J_tel_handles, J_tel_labels = get_unique_legend(J_tel_axis)
-        J_tel_figure.suptitle('Teleported operators expectation Values for Different N and J', fontsize=14)
-        J_tel_figure.tight_layout(rect=[0, 0.05, 1, 0.93])
-        J_tel_figure.legend(J_tel_handles, J_tel_labels, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
-        J_tel_figure.savefig(f'artifacts/teleported_operators_{name}_vs_J.png', bbox_inches='tight')
-        plt.close(J_tel_figure)
+            J_tel_handles, J_tel_labels = get_unique_legend(J_tel_axis)
+            J_tel_figure.suptitle('Teleported operators expectation Values for Different N and J', fontsize=14)
+            J_tel_figure.tight_layout(rect=[0, 0.05, 1, 0.93])
+            J_tel_figure.legend(J_tel_handles, J_tel_labels, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
+            J_tel_figure.savefig(f'artifacts/teleported_operators_{run_conf.name}_{alice_base}_vs_J.png', bbox_inches='tight')
+            plt.close(J_tel_figure)
 
-        h_tel_handles, h_tel_labels = get_unique_legend(h_tel_axis)
-        h_tel_figure.suptitle('Teleported operators expectation Values for Different N and h', fontsize=14)
-        h_tel_figure.tight_layout(rect=[0, 0.05, 1, 0.93])
-        h_tel_figure.legend(h_tel_handles, h_tel_labels, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
-        h_tel_figure.savefig(f'artifacts/teleported_operators_{name}_vs_h.png', bbox_inches='tight')
-        plt.close(h_tel_figure)
+            h_tel_handles, h_tel_labels = get_unique_legend(h_tel_axis)
+            h_tel_figure.suptitle('Teleported operators expectation Values for Different N and h', fontsize=14)
+            h_tel_figure.tight_layout(rect=[0, 0.05, 1, 0.93])
+            h_tel_figure.legend(h_tel_handles, h_tel_labels, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
+            h_tel_figure.savefig(f'artifacts/teleported_operators_{run_conf.name}_{alice_base}_vs_h.png', bbox_inches='tight')
+            plt.close(h_tel_figure)
 
-        for err_name, (err_figure, err_axis) in errs_plot.items():
-            err_handles, err_labels = get_unique_legend(err_axis)
-            err_figure.suptitle(f'Teleported value vs. {err_name} for different Js', fontsize=14)
-            err_figure.tight_layout(rect=[0, 0.05, 1, 0.93])
-            err_figure.legend(err_handles, err_labels, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
-            os.makedirs(f'artifacts/errors/{name}', exist_ok=True) 
-            err_figure.savefig(f'artifacts/errors/{name}/{err_name}.png', bbox_inches='tight')
-            plt.close(err_figure)
+            for err_name, (err_figure, err_axis) in errs_plot.items():
+                err_handles, err_labels = get_unique_legend(err_axis)
+                err_figure.suptitle(f'Teleported value vs. {err_name} for different Js', fontsize=14)
+                err_figure.tight_layout(rect=[0, 0.05, 1, 0.93])
+                err_figure.legend(err_handles, err_labels, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
+                os.makedirs(f'artifacts/errors/{run_conf.name}_{alice_base}', exist_ok=True) 
+                err_figure.savefig(f'artifacts/errors/{run_conf.name}_{alice_base}/{err_name}.png', bbox_inches='tight')
+                plt.close(err_figure)
+
+        J_tel_handles_both_bases, J_tel_labels_both_bases = get_unique_legend(J_tel_axis_both_bases)
+        J_tel_figure_both_bases.suptitle('Teleported operators expectation Values for Different N and J', fontsize=14)
+        J_tel_figure_both_bases.tight_layout(rect=[0, 0.05, 1, 0.93])
+        J_tel_figure_both_bases.legend(J_tel_handles_both_bases, J_tel_labels_both_bases, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
+        J_tel_figure_both_bases.savefig(f'artifacts/teleported_operators_{run_conf.name}_vs_J.png', bbox_inches='tight')
+        plt.close(J_tel_figure_both_bases)
+
+        h_tel_handles_both_bases, h_tel_labels_both_bases = get_unique_legend(h_tel_axis_both_bases)
+        h_tel_figure_both_bases.suptitle('Teleported operators expectation Values for Different N and h', fontsize=14)
+        h_tel_figure_both_bases.tight_layout(rect=[0, 0.05, 1, 0.93])
+        h_tel_figure_both_bases.legend(h_tel_handles_both_bases, h_tel_labels_both_bases, loc='upper center', bbox_to_anchor=(0.5, -0.02), ncol=3)
+        h_tel_figure_both_bases.savefig(f'artifacts/teleported_operators_{run_conf.name}_vs_h.png', bbox_inches='tight')
+        plt.close(h_tel_figure_both_bases)
