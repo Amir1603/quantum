502dca8bc215ab234fda9324e675623dcd249385
Amir Yona <yonaamir@gmail.com>
2025-08-21 23:25:16 +0300

    Organize plots in numerical simulation in separate files and according to well defined directory structure with same conventions as qiskit simulation

--- ibm/numerical_run_conf.py
+++ ibm/numerical_run_conf.py
@@ -1,7 +1,42 @@
 from utils import AliceBase
 from Calculators import Operators
 from conf import ErrorsConf
+from plot_utils import PlotProperties
 
+
+class RunConf:
+    def __init__(self, name: str, H_type: type, OB_types: dict[str, type], alice_basis: AliceBase, N: int):
+        self.name = name
+        self.H_type = H_type
+        self.OB_types = OB_types
+        self.alice_basis = alice_basis
+        self.N = N
+
+        self.J_pps = {
+            k: PlotProperties(
+                f"{self.name}/{self.alice_basis}/N{self.N}/{k}_vs_J.png"
+            ) for k in self.OB_types.keys()
+        }
+        self.h_pps = {
+            k: PlotProperties(
+                f"{self.name}/{self.alice_basis}/N{self.N}/{k}_vs_h.png", True
+            ) for k in self.OB_types.keys()
+        }
+        self.errs_pps = {k: {e: PlotProperties(
+                f"{self.name}/{self.alice_basis}/N{self.N}/{k}_vs_{e}.png"
+            ) for e in NumericalRunConf.get_errors()} for k in self.OB_types.keys()}
+
+    def get_all_pps(self) -> list[PlotProperties]:
+        """
+        Returns a list of all PlotProperties objects for this RunConf.
+        """
+        pps = []
+
+        pps.extend(self.J_pps.values())
+        pps.extend(self.h_pps.values())
+        [pps.extend(v.values()) for v in self.errs_pps.values()]
+
+        return pps
 
 class NumericalRunConf:
     @staticmethod
@@ -9,7 +44,7 @@ class NumericalRunConf:
         return NumericalRunConf(
             'alice',
             Operators.alice_H,
-            [Operators.alice_HB, Operators.QB],
+            {"energy": Operators.alice_HB, "charge": Operators.QB},
             [AliceBase.X],
             [1, 2, 3, 4]
         )
@@ -19,20 +54,20 @@ class NumericalRunConf:
         return NumericalRunConf(
             'nn',
             Operators.nn_H,
-            [Operators.nn_HB, Operators.QB],
+            {"energy": Operators.nn_HB, "charge": Operators.QB},
             [AliceBase.X, AliceBase.Y],
             [2, 3, 4]
         )
 
     @staticmethod
-    def get_ylim(error_name: str, OB_type: str):
-        if OB_type == 'alice_HB':
+    def get_ylim(error_name: str, H_type: type):
+        if H_type == Operators.alice_H:
             if error_name == 'p_bitflip_error': return (-0.1, 0.5)
             if error_name == 'p_alice_phaseflip_error': return (-0.1, 0.5)
             if error_name == 'p_bob_phaseflip_error': return (-0.1, 0.5)
             if error_name == 'p_excited_mixture_error': return (-0.1, 0.2)
             if error_name == 'p_excited_superposition_error': return (-0.1, 0.2)
-        elif OB_type == 'nn_HB':
+        elif H_type == Operators.nn_H:
             if error_name == 'p_bitflip_error': return (-0.1, 0.5)
             if error_name == 'p_bob_phaseflip_error': return (-0.1, 0.5)
 
@@ -60,9 +95,7 @@ class NumericalRunConf:
             'p_excited_superposition_error',
         ]
 
-    def __init__(self, name: str, H_type: type, OB_types: list[type], alice_bases: list[AliceBase], Ns: list[int]):
-        self.name = name
-        self.H_type = H_type
-        self.OB_types = OB_types
-        self.alice_bases = alice_bases
-        self.Ns = Ns
+    def __init__(self, name: str, H_type: type, OB_types: dict[str, type], alice_bases: list[AliceBase], Ns: list[int]):
+        self.confs = [RunConf(name, H_type, OB_types, alice_base, N)
+                      for alice_base in alice_bases
+                      for N in Ns]
